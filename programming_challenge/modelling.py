1228370af063c4db76df56e781cb4618b227b49a
Mattia Evangelisti <71966068+MadMatti@users.noreply.github.com>
2022-10-18 08:51:31 +0200

    Adding models

--- programming_challenge/modelling.py
+++ programming_challenge/modelling.py
@@ -7,7 +7,8 @@ from sklearn.decomposition import PCA
 from sklearn.compose import ColumnTransformer
 
 from sklearn.model_selection import cross_val_score, StratifiedKFold
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from sklearn.model_selection import cross_val_predict
@@ -49,11 +50,11 @@ def test_classifiers(preprocessor, XY_t):
 
     '''Find the best parameters for the random forest classifier'''
     '''Random Forest'''
-    pipeline = Pipeline(steps=[('preprocessor', preprocessor),
+    '''pipeline = Pipeline(steps=[('preprocessor', preprocessor),
                             ('standardscaler', StandardScaler()),
                            ('forest', RandomForestClassifier(random_state=R))])
 
-    params = { 
+    params_forest = { 
     'forest__bootstrap': [True, False],
     'forest__max_depth': [10, 20, 30, 40, 45, 50, 55, 60, 65, 70, 75, 80, 90, 100, None],
     'forest__max_features': ['sqrt', 'log2'],
@@ -62,31 +63,55 @@ def test_classifiers(preprocessor, XY_t):
     'forest__n_estimators': [200, 600, 800, 850, 875, 900, 950, 975, 1000, 1100, 1200, 1400, 1600, 1800, 2000, 2200, 2400],
     }
 
-    # forest_search = RandomizedSearchCV(pipeline, param_distributions=params, n_iter=240, verbose=1, n_jobs=-1, cv=cv)
-    # forest_search.fit(X_t, Y_t)
-    # print(forest_search.best_params_)
-    # print(forest_search.best_score_)
+    forest_search = RandomizedSearchCV(pipeline, param_distributions=params_forest, n_iter=240, verbose=1, n_jobs=-1, cv=cv)
+    forest_search.fit(X_t, Y_t)
+    print(forest_search.best_params_)
+    print(forest_search.best_score_)'''
 
-    max_depth = list(range(1,101, 2))
-    min_samples_l = list(range(1,21, 2))
-    min_samples_s = list(range(2,51, 2))
-    estim = list(range(100, 5000, 20))
+    # max_depth = list(range(1,101, 2))
+    # min_samples_l = list(range(1,21, 2))
+    # min_samples_s = list(range(2,51, 2))
+    # estim = list(range(100, 5000, 20))
 
-    params_b = {
-    'forest__bootstrap': [True, False],
-    'forest__max_depth': max_depth,
-    'forest__max_features': ['sqrt', 'log2'],
-    'forest__min_samples_leaf': min_samples_l,
-    'forest__min_samples_split': min_samples_s,
-    'forest__n_estimators': estim,
+    # params_b = {
+    # 'forest__bootstrap': [True, False],
+    # 'forest__max_depth': max_depth,
+    # 'forest__max_features': ['sqrt', 'log2'],
+    # 'forest__min_samples_leaf': min_samples_l,
+    # 'forest__min_samples_split': min_samples_s,
+    # 'forest__n_estimators': estim,
+    # }
+
+    # forest_search_b = BayesSearchCV(pipeline, search_spaces=params_b, n_iter=240, verbose=1, n_jobs=-1, cv=cv)
+    # forest_search_b.fit(X_t, Y_t)
+    # print(forest_search_b.best_params_)
+    # print(forest_search_b.best_score_)
+
+    '''SVM'''
+    svc = Pipeline(steps=[('preprocessor', preprocessor),
+                            ('standardscaler', StandardScaler()),
+                           ('svc', SVC(random_state=R, kernel='rbf', C=1.0, gamma='auto', probability=True))])
+
+    svc.fit(X_t, Y_t)
+    print(np.average(cross_val_score(svc, X_t, Y_t, cv=cv)))
+
+    pipeline_gradient = Pipeline(steps=[('preprocessor', preprocessor),
+                            ('standardscaler', StandardScaler()),
+                            ('gb', GradientBoostingClassifier(random_state=R))])
+    
+    params_gradient = {
+        "gb__n_estimators" : [50, 75, 100, 150, 200, 250, 300, 400, 500],
+        "gb__max_depth" : [5, 7, 10, 15, 20, None],
+        "gb__learning_rate" : [0.1, 0.2, 0.01, 0.05, 0.075],
     }
 
-    forest_search_b = BayesSearchCV(pipeline, search_spaces=params_b, n_iter=240, verbose=1, n_jobs=-1, cv=cv)
-    forest_search_b.fit(X_t, Y_t)
-    print(forest_search_b.best_params_)
-    print(forest_search_b.best_score_)
+    gradient_search = RandomizedSearchCV(pipeline_gradient, param_distributions=params_gradient, n_iter=120, verbose=1, n_jobs=-1, cv=cv)
+    gradient_search.fit(X_t, Y_t)
+    print(gradient_search.best_params_)
+    print(gradient_search.best_score_)
 
-    return forest_search_b
+
+    return gradient_search
 
 def test_model(model, X, Y, file):
     cv_test = StratifiedKFold(n_splits=5, random_state=(R + 1), shuffle=True)
